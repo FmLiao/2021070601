fc5882275f6ecc9dd1fa9283275c396e9f2e218c
mingliao <ming.liao@msa.hinet.net>
2021-07-07 15:19:10 +0800

    Initial Commit

---
+++ 2021070601/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2021070601
+//
+//  Created by LIAO FU MING on 2021/7/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
